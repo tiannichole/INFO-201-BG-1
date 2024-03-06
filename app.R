04dd2fd8dc89328bb34e0cf7a16d3e792ef7f97a
tiannichole <128257308+tiannichole@users.noreply.github.com>
2024-03-05 17:17:30 -0800

    FINAL

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
  install.packages("shiny")
- install.packages ("shinyWidgets")
+ install.packages("shinyWidgets")
  install.packages("ggplot2")
  install.packages("tidyverse")
  install.packages("lubridate")
@@ -7,22 +7,32 @@
  install.packages("plotly")
  install.packages("data.table")
  install.packages("dplyr")
+ install.packages("rsconnect")
+ install.packages("read.r")
+ install.packages("bs.lib")
+ install.pcakages("tidyverse")
+install.packages("RColorBrewer")
+install.packages("scales")
+ 
+ library(tidyverse)
+ library(lubridate)
+ library(eeptools)
+ library(shiny)
+ library(shinyWidgets)
+ library(ggplot2)
+ library(plotly)
+ library(data.table)
+ library(dplyr)
+ library(rsconnect)
+ library(bslib)
 
 
-library(tidyverse)
-library(lubridate)
-library(eeptools)
 library(shiny)
-library (shinyWidgets)
-library(ggplot2)
-library(plotly)
-library(data.table)
-library(dplyr)
-
 
 source("ui.R")
 source("server.R")
 
+library(rsconnect)
 
 
 shinyApp(ui = ui, server = server)
